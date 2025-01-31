3e5c38c584287576e8c13846e9ad59ea4dac6454
Eric J. Meier <ericmeier@live.com>
2025-01-31 13:06:13 -0700

    Changed the tribal cards to non-stacking tokens and rejiggered everything to prepare the deck. Also modified the play button some. Added dealing to all seated players to beginning of script

--- .tts/Prepare Deck Button.6e412c.lua
+++ .tts/Prepare Deck Button.6e412c.lua
@@ -3,86 +3,89 @@ self.interactable = false
 self.tooltip = false
 
 function click_func(player,value,id)
-    singleDeck=getObjectFromGUID("57a5ca")
-    doubleDeck=getObjectFromGUID("5eb4b9")
 
-    --n = #getSeatedPlayers()
-    n=5
+    function prep_deck(singles,doubles)
+
+        destroyObject(self)
+        tribalBag=spawnObject({type="Bag",position={0,1,15}})
+
+        for i=1,singles do
+            tribalBag.putObject(singleTribals[i])
+        end
+
+        for i=1,doubles do
+            tribalBag.putObject(doubleTribals[i])
+        end
+
+        tribalBag.shuffle()
+        
+        for i,obj in ipairs(singleTribals) do
+            destroyObject(obj)
+        end
+
+        for i,obj in ipairs(doubleTribals) do
+            destroyObject(obj)
+        end
+
+        tribals = tribalBag.getQuantity()
+        destroyObject(tribalBag)
+        mainDecks=mainDeck.split(tribals)
+        Physics.setGravity({0,-5,0})
+        for i = 1, tribals do
+            tribalBag.takeObject({smooth=false,position={5,(i-1)*0.5+1,0}}).flip()
+            mainDecks[i].setPosition({5,(i-1)*0.5+1.25,0})
+        end
+        for i = 1, tribals do
+            mainDecks[i].setRotation({-180,0,0})
+        end
+        Wait.time(function() 
+            Physics.setGravity({0,-25,0}) 
+        end, 1)
+    end
+
+    n = #getSeatedPlayers()
+    -- n=3
+
     if n < 3 then
-        broadcastToAll("You need 3 people to play!")
-    else
-        broadcastToAll("Preparing Deck for "..n.." Players")
-        mainDeck=getObjectFromGUID("67530b")
-        mainDeck.shuffle()
+        broadcastToAll("Minimum of 3 players!")
+    else 
+        if n > 6 then
+            broadcastToAll("Maximum of 6 players!")
+        else
+            broadcastToAll("Preparing Deck for "..n.." Players")
+            mainDeck=getObjectFromGUID("67530b")
+            mainDeck.shuffle()
+            mainDeck.deal(3)
+    
+            singleTribals={
+                getObjectFromGUID("e0df29"),
+                getObjectFromGUID("e0c5d8"),
+                getObjectFromGUID("dc242d"),
+                getObjectFromGUID("09747e")
+            }
+            doubleTribals={
+                getObjectFromGUID("8d0458"),
+                getObjectFromGUID("8f4551"),
+                getObjectFromGUID("22d324"),
+                getObjectFromGUID("2a3f11"),
+                getObjectFromGUID("7d0824")
+            }
+            tribalBag=getObjectFromGUID("1d5654")
+        end
     end
 
     if n==3 then
-        tribalDeck = singleDeck
-        tribals = tribalDeck.getQuantity()
-        mainDecks=mainDeck.split(tribals)
-        for i = 1, tribals do
-            mainDecks[i] = mainDecks[i].putObject(tribalDeck.takeObject({smooth=false}))
-        end
-        for i = 2, tribals do
-            mainDeck = mainDecks[1].putObject(mainDecks[i])
-        end
-        mainDeck.rotate({0,-90,0})
-        destroyObject(singleDeck)
-        destroyObject(doubleDeck)
-        destroyObject(self)
+        prep_deck(4,0)
     end
     if n==4 then
-        tribalDeck = singleDeck.takeObject({position={0,0,-5},smooth=false})
-        tribalDeck = tribalDeck.putObject(singleDeck.takeObject({smooth=false}))
-        tribalDeck = tribalDeck.putObject(doubleDeck.takeObject({smooth=false}))
-        tribalDeck = tribalDeck.putObject(doubleDeck.takeObject({smooth=false}))
-        tribalDeck.shuffle()
-        tribals = tribalDeck.getQuantity()
-        mainDecks=mainDeck.split(tribals)
-        for i = 1, tribals do
-            mainDecks[i] = mainDecks[i].putObject(tribalDeck.takeObject({smooth=false}))
-        end
-        for i = 2, tribals do
-            mainDeck = mainDecks[1].putObject(mainDecks[i])
-        end
-        mainDeck.rotate({0,-90,0})
-        destroyObject(singleDeck)
-        destroyObject(doubleDeck)
-        destroyObject(self)
+        prep_deck(2,2)
     end
+
     if n==5 then
-        tribalDeck = singleDeck.takeObject({position={0,0,-5},smooth=false})
-        tribalDeck = tribalDeck.putObject(singleDeck.takeObject({smooth=false}))
-        tribalDeck = tribalDeck.putObject(doubleDeck.takeObject({smooth=false}))
-        tribalDeck = tribalDeck.putObject(doubleDeck.takeObject({smooth=false}))
-        tribalDeck = tribalDeck.putObject(doubleDeck.takeObject({smooth=false}))
-        tribalDeck.shuffle()
-        tribals = tribalDeck.getQuantity()
-        mainDecks=mainDeck.split(tribals)
-        for i = 1, tribals do
-            mainDecks[i] = mainDecks[i].putObject(tribalDeck.takeObject({smooth=false}))
-        end
-        for i = 2, tribals do
-            mainDeck = mainDecks[1].putObject(mainDecks[i])
-        end
-        mainDeck.rotate({0,-90,0})
-        destroyObject(singleDeck)
-        destroyObject(doubleDeck)
-        destroyObject(self)
+        prep_deck(2,3)
     end
+
     if n==6 then
-        tribalDeck = doubleDeck
-        tribals = tribalDeck.getQuantity()
-        mainDecks=mainDeck.split(tribals)
-        for i = 1, tribals do
-            mainDecks[i] = mainDecks[i].putObject(tribalDeck.takeObject({smooth=false}))
-        end
-        for i = 2, tribals do
-            mainDeck = mainDecks[1].putObject(mainDecks[i])
-        end
-        mainDeck.rotate({0,-90,0})
-        destroyObject(singleDeck)
-        destroyObject(doubleDeck)
-        destroyObject(self)
+        prep_deck(0,5)
     end
 end
